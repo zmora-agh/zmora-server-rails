726cac7ee1fe0a9df63fee6755ccc19e5c2a88b5
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-21 18:48:46 +0200

    Basic authorization

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,6 +1,8 @@
 class GraphqlController < ApplicationController
+  before_action :authenticate_request!
+
   def query
-    result = Schema.execute(params[:query], variables: params[:variables])
+    result = Schema.execute(params[:query], variables: params[:variables], context: @current_user)
     render json: result
   end
 end
