e7f94e88e1ceaec211f9b9907472255eb544ce29
Marcin Flis <marcin.flis.95@gmail.com>
2017-08-10 21:20:53 +0200

    contest problem. results test - passing example

--- spec/models/contest_problem_spec.rb
+++ spec/models/contest_problem_spec.rb
@@ -4,36 +4,29 @@ RSpec.describe ContestProblem, type: :model do
   describe '#results' do
     before(:each) do
 
-      # values are arrays. Allows to store multiple values for tha same key
-      @submit_map = Hash.new {|hsh, key| hsh[key] = []}
-
       @contest_problem = create(:contest_problem)
       @contest = @contest_problem.contest
 
       @mentor1 = create(:user)
 
       @part1 = create(:contest_participation, contest: @contest, contest_owner: @mentor1)
+      @status ={:ok => 0, :que => 1, :err => 2}
 
-      create_submit(@part1, @contest_problem)
-      create_submit(@part1, @contest_problem)
+      submit11 = method(:create_submit).curry[@part1][@contest_problem]
 
-      #debugging
-      puts("mentor #{@mentor1.id}")
-      puts("paticipation: user: #{@part1.user.id}, mentor: #{@part1.contest_owner.id}")
-      @submit_map[@part1.contest_owner.id].map {|s| puts("id:#{s.id} , author: #{s.author.id} , problem:#{s.contest_problem.id}")}
-
+      @newer = submit11.curry[@status[:ok]][Date.new(2017, 8, 10)]
+      @older= submit11.curry[@status[:ok]][Date.new(2017, 8, 9)]
+      @wrong= submit11.curry[@status[:err]][Date.new(2017, 8, 9)]
     end
 
-    it 'should find multiple submits from one user to one contest_problem' do
-      expect(@contest_problem.results(@mentor1.id)).to match_array @submit_map[@mentor1.id]
+    it 'should find only  newest submit with OK status from one user to one contest_problem' do
+      expect(@contest_problem.results(@mentor1.id)).to contain_exactly (@newer)
 
     end
   end
 
-  # helper methods
-  def create_submit(participation, problem)
-    submit = create(:submit, contest_problem: problem, author: participation.user)
-    @submit_map[participation.contest_owner.id].push submit
+  def create_submit(participation, problem, stat, created_at)
+    create(:submit, contest_problem: problem, author: participation.user, status: stat, created_at: created_at)
   end
 
 end
