54f94098768812da7e4f48deab71e98f932d8ece
Marcin Flis <marcin.flis.95@gmail.com>
2017-08-14 19:42:53 +0200

    contest participation factory - create ownership only if does not already exists

--- spec/factories/contest_participation_factory.rb
+++ spec/factories/contest_participation_factory.rb
@@ -1,17 +1,15 @@
 FactoryGirl.define do
   factory :contest_participation do
-    transient do
-      create_ownership true
-    end
 
     contest
     user
     association :contest_owner, factory: :user
 
-    after(:create) do |contest_participation, evaluator|
-      if evaluator.create_ownership
-        create(:contest_ownership, owner: contest_participation.contest_owner,
-                                   contest: contest_participation.contest)
+    after(:create) do |participation, _|
+      unless ContestOwnership.find_by(contest: participation.contest, owner: participation.contest_owner)
+
+        create(:contest_ownership, owner: participation.contest_owner,
+               contest: participation.contest)
       end
     end
   end
