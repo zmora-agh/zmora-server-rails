bb3ec63ba94b41ed2bdfebb1bccb74f9f99a5801
Mateusz Nowotyński <maxmati@memleak.pl>
2017-08-19 23:00:59 +0200

    Move task result to error queue when unable to update database
    Minor changes:
     * enable manual ack mode

--- lib/rabbitmq.rb
+++ lib/rabbitmq.rb
@@ -1,20 +1,29 @@
 require 'bunny'
 
 class RabbitMQ
-  def initialize(quename, uri = 'amqp://localhost')
+  def initialize(quename, uri = 'amqp://localhost', error_quename = nil)
     @con = Bunny.new(uri)
     @con.start
     @ch = @con.create_channel
     @que = @ch.queue(quename, durable: true)
+    @err_que = @ch.queue(error_quename, durable: true) if error_quename
   end
 
   def send(message)
     @ch.default_exchange.publish(message, routing_key: @que.name)
   end
 
+  def send_error(message)
+    @ch.default_exchange.publish(message, routing_key: @err_que.name)
+  end
+
+  def ack(delivery_tag)
+    @ch.ack(delivery_tag, false)
+  end
+
   def start_receiving
-    @que.subscribe do |_delivery_info, _metadata, payload|
-      yield payload
+    @que.subscribe(manual_ack: true) do |delivery_info, _metadata, payload|
+      yield payload, delivery_info
     end
   end
 
