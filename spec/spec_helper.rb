a8a4a0ab1bc9b784c1d80016d38adff2e16bce05
Marcin Moskal <mmos@memleak.pl>
2017-09-19 17:59:47 +0200

    Describe currently running test, style fix
    Meaningful description is displayed as output text in RubyMine for unnamed it()
    blocks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@
 require "shoulda/matchers"
 
 RSpec.configure do |config|
+  config.formatter = :documentation
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
